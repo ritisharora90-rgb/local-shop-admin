becff812049fdeb5685d80e1f48558cdce62e676
ritisharora90-rgb <ritisharora90@gmail.com>
2026-06-23 11:42:42 +0530

    Add MongoDB cart storage

---
+++ app/Models/Cart.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use MongoDB\Laravel\Eloquent\Model;
+
+class Cart extends Model
+{
+    protected $connection = 'mongodb';
+
+    protected $collection = 'carts';
+
+    protected $fillable = [
+        'user_id',
+        'product_id',
+        'quantity'
+    ];
+}
--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
+use App\Models\Cart;
+use Illuminate\Http\Request;
 
+
+// Products
 Route::get(
 '/products',
 [ProductController::class,'apiIndex']
@@ -11,4 +15,49 @@ Route::get(
 Route::get(
 '/products/{id}',
 [ProductController::class,'show']
+);
+
+
+// Get Cart
+Route::get(
+'/cart/{user}',
+function($user){
+
+return Cart::where(
+'user_id',
+$user
+)->first()
+
+?? [
+
+'items'=>[]
+
+];
+
+}
+);
+
+
+// Save Cart
+Route::post(
+'/cart',
+function(Request $request){
+
+$cart =
+Cart::firstOrCreate(
+[
+'user_id'=>'guest'
+]
+);
+
+$cart->items =
+$request->items;
+
+$cart->save();
+
+return response()->json(
+$cart
+);
+
+}
 );
